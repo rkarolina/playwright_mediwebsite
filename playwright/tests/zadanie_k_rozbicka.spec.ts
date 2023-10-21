9155f00491a4b26b6797417822e540c66e398d9b
Karolina Rozbicka <135992492+rkarolina@users.noreply.github.com>
2023-10-21 23:11:06 +0200

    Update zadanie_k_rozbicka.spec.ts

--- playwright/tests/zadanie_k_rozbicka.spec.ts
+++ playwright/tests/zadanie_k_rozbicka.spec.ts
@@ -4,31 +4,31 @@ test.setTimeout(120000);
 
 test("has title2", async ({ page }) => {
   // go to website
-  await page.goto("https://telemedi.com/pl/");
+  await page.goto("https://****medi.com/pl/");
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Lekarz Online/);
 });
 
 test("get started link", async ({ page }) => {
-  await page.goto("https://telemedi.com/pl/");
+  await page.goto("https://****medi.com/pl/");
 
   // accept cookie
   await page.getByRole("button", { name: "Zezwól na wszystkie" }).click();
   // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*telemedi/);
+  await expect(page).toHaveURL(/.*****medi/);
 });
 
 test("has title login page", async ({ page }) => {
   // go to website
-  await page.goto("https://panel.telemedi.com/pl/login");
+  await page.goto("https://panel.****medi.com/pl/login");
 
   await expect(page).toHaveTitle("Konto pacjenta");
 });
 
 test("login", async ({ page }) => {
-  await page.goto("https://telemedi.com/pl/");
+  await page.goto("https://****medi.com/pl/");
   await page.getByRole("button", { name: "Zezwól na wszystkie" }).click();
-  // await page.goto("https://panel.telemedi.com/pl/login");
+  // await page.goto("https://panel.****medi.com/pl/login");
   await page.getByRole("link", { name: "Zaloguj się" }).click();
   // logowanie
   await page.getByPlaceholder("E-mail, PESEL lub identyfikator").click();
@@ -45,13 +45,13 @@ test("login", async ({ page }) => {
 
 test("has title make consultation page", async ({ page }) => {
   // go to website
-  await page.goto("https://panel.telemedi.com/pl");
+  await page.goto("https://panel.****medi.com/pl");
 
   await expect(page).toHaveTitle("Konto pacjenta");
 });
 
 test("makeConsultation", async ({ page }) => {
-  await page.goto("https://panel.telemedi.com/pl");
+  await page.goto("https://panel.****medi.com/pl");
   await page.getByRole("button", { name: "Umów się" }).click();
   await page
     .getByRole("button", { name: "Recepta Konsultacja z ankietą medyczną" })
@@ -61,8 +61,8 @@ test("makeConsultation", async ({ page }) => {
 //---------------------
 
 test("zadanie calosc", async ({ page }) => {
-  // Przejście na adres: https://telemedi.com/pl/
-  await page.goto("https://telemedi.com/pl/");
+  // Przejście na adres: https://****medi.com/pl/
+  await page.goto("https://****medi.com/pl/");
 
   const LogIn = await page.getByRole("link", { name: "Zaloguj się" });
   const cookies = await page.getByRole("button", {
@@ -79,9 +79,9 @@ test("zadanie calosc", async ({ page }) => {
   await page.getByPlaceholder("E-mail, PESEL lub identyfikator").click();
   await page
     .getByPlaceholder("E-mail, PESEL lub identyfikator")
-    .fill("telemeditest@gmail.com");
+    .fill("****meditest@gmail.com");
   await page.getByPlaceholder("Hasło").click();
-  await page.getByPlaceholder("Hasło").fill("Telemeditest12!");
+  await page.getByPlaceholder("Hasło").fill("****meditest12!");
 
   // Zaloguj się button located using xpath
   await page.locator("xpath=//div[3]/button").click();
@@ -121,8 +121,8 @@ test("zadanie calosc", async ({ page }) => {
   await page.getByRole("button", { name: "Wybierz" }).click();
 
   //waiting until Checkbox section is visible
-  const checkboxTelemediGO = await page.locator("div.telemedi-go");
-  await checkboxTelemediGO.click();
+  const checkbox****mediGO = await page.locator("div.****medi-go");
+  await checkbox****mediGO.click();
   const label = await page.locator("[for='checkAll']");
   await label.click();
 
